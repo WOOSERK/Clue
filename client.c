5bcc61b3665ef9481c38e029d2f1f240d2cc4d2d
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2020-07-09 07:10:24 -0700

    v1.0.0.0.1

--- client.c
+++ client.c
@@ -11,20 +11,19 @@
 
 #include "ncur.h"
 #include "clue.h"
-
-
+#define ADDRESS ("192.168.30.31")
 int client_connect(void);
 int game_init(int sock, int *player_id, WINDOW ***windows,Player_packet *packet);    
 int game_update(int sock, WINDOW ***windows,Cursor *cursor);
 int game_play(int sock, int player_id,Player_packet *packet);	 
-int roll_and_go(int sock, int player_id,WINDOW ***windows,Player_packet *packet);
+int roll_and_go(int sock, int player_id,WINDOW ***windows,Player_packet *packet,Cursor *cursor);
 int roll_dice(void);
 int return_player_choice(int dice_value);
 int return_player_position(int choice, int* y, int* x);
 int set_dice_in_packet(Player_packet* packet, int dice_value, int choice_value);
 int set_player_in_packet(Player_packet* packet, int player_id, int y, int x);
 int sig_recv(int sock);
-int game_infer(int sock, int player_id,Cursor* cursor,WINDOW ***windows);
+int game_infer(int sock, int player_id,Cursor* cursor,WINDOW ***windows, pthread_t tid);
 int send_clue(int sock, Player_packet *packet,Cursor *cursor,WINDOW ***windows);
 int print_clue(Player_packet *packet,Cursor *cursor);
 
@@ -60,12 +59,13 @@ int main(){
 		printf("사건이 해결되었다!!\n");
 	}
 
+	endwin();
 	return 0;	
 }
 
 
 int client_connect(void){
-	int sock = socket(PF_INET, SOCK_STREAM, 0); 
+int sock = socket(PF_INET, SOCK_STREAM, 0); 
 	if(sock == -1){
 		perror("socket");
 		return -1;
@@ -74,7 +74,7 @@ int client_connect(void){
 	struct sockaddr_in addr = {0,};
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(8080);
-	addr.sin_addr.s_addr = inet_addr("192.168.30.31"); // 서버주소
+	addr.sin_addr.s_addr = inet_addr(ADDRESS); // 서버주소
 	
 	if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1){
 		perror("connect");
@@ -86,7 +86,7 @@ int client_connect(void){
 
 int game_play(int sock, int player_id, Player_packet *packet) {  	
 	
-	WINDOW ***windows = display_init(packet->cards);	
+	WINDOW ***windows = display_init(packet->cards,player_id);	
 	Cursor cursor;
 	cursor.history = windows[6];
 	cursor.log = windows[7];
@@ -95,23 +95,29 @@ int game_play(int sock, int player_id, Player_packet *packet) {
 	cursor.command = windows[13][2]; 
 	// 이후 클라이언트는 packet_recv로 대기한다. 
 	// 서버는 SIG_TURN 또는 SIG_WAIT을 보낼 것이다.
+	
+
 	while(1){
 		int sig;
 		int type;
 
-		int turn_player = (int)PLAYER_TURN_PLAYER(packet->info);
+		int turn_player = (int)PLAYER_TURN_PLAYER(packet->info) >>4;
 		blink_player(windows[12],turn_player);
 		char buf[32];
-		sprintf(buf,"player %d turn",turn_player);
-		str_add(cursor.log_str,((cursor.log_cnt)+=1),buf);
+		str_add(cursor.log_str,(cursor.log_cnt)++,buf);
 
 		if (packet_recv(sock,(char*)&sig, &type) == -1) {
 			return -1;
 		}
 
+		pthread_t pid;
 		// 턴플레이어의 경우
 		if (sig == SIG_TURN){
-			roll_and_go(sock, player_id, windows,packet);
+			roll_and_go(sock, player_id, windows,packet,&cursor);
+		}
+		else
+		{
+			pthread_create(&pid,NULL,move_cursor,(void*)&cursor);
 		}
 
 		// 턴 플레이어의 roll_and_go 정보를 모든 플레이어가 받아서 업데이트
@@ -119,7 +125,7 @@ int game_play(int sock, int player_id, Player_packet *packet) {
 			return -1;
 		}
 
-		int result = game_infer(sock, player_id,&cursor,windows); 
+		int result = game_infer(sock, player_id,&cursor,windows, pid);
 		if (result == SIG_WIN) {
 			return SIG_WIN;
 		}
@@ -134,7 +140,7 @@ int game_play(int sock, int player_id, Player_packet *packet) {
 	return 0;
 }
 
-int roll_and_go(int sock, int player_id, WINDOW ***windows, Player_packet *packet) {
+int roll_and_go(int sock, int player_id, WINDOW ***windows, Player_packet *packet,Cursor *cursor) {
 	
 	WINDOW **player ;
 	return_player_horse(windows, &player, player_id);
@@ -143,17 +149,30 @@ int roll_and_go(int sock, int player_id, WINDOW ***windows, Player_packet *packe
 	int dice_value = 0;
 	int choice_value = 0;
 
-	unsigned short position = PLAYER_POSITION(packet->position,player_id) >> (4 * (3 - player_id));
+	unsigned short position =(unsigned short)(PLAYER_POSITION(packet->position,player_id)) >> (4 * (3 - player_id));
+
 	return_yx(position, &y, &x);
 
 	// 주사위값 얻어냄
+	char dice_buf[BUFSIZ];
+	sprintf(dice_buf,"press 'y' to roll dice ");
+	str_add(cursor->log_str,(cursor->log_cnt)++,dice_buf);
+	history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
+
 	dice_cursor(windows[12][5]);
 	dice_value = roll_dice();
 	dice_num_print(windows[13][1],dice_value);
 
-	// 선택값 얻어냄
-	choice_value = map_cursor(player,windows[12],player_id,dice_value,y,x);
-
+	if( (y == 3&&x==2) || dice_value != 1){
+		char location_buf[BUFSIZ];
+		sprintf(location_buf,"press arrow keys and 's' to select location ");
+		str_add(cursor->log_str,(cursor->log_cnt)++,location_buf);
+	history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
+		choice_value = map_cursor(player,windows[12],player_id,dice_value,y,x);
+	}
+	else{
+		choice_value = PLAYER_SELECT_VALUE(packet->dice); 
+	}
 	// 위치값 얻어냄
 	if(return_player_position(choice_value, &y, &x) == -1){
 		return -1;	 
@@ -163,7 +182,7 @@ int roll_and_go(int sock, int player_id, WINDOW ***windows, Player_packet *packe
 	set_player_in_packet(packet, player_id, y, x);
 
 	int type = PACKET;
-
+	unsigned short us = packet->position;
 	// 서버에게 roll_and_go 정보를 전송
 	packet_send(sock, (char*)packet, &type);
 
@@ -179,6 +198,7 @@ int return_player_choice(int dice_value){
 	
 	// 선택값을 리턴하는 함수
 	int choice_value = -1;
+
 	while(1){
 		if(choice_value <= dice_value){
 			break;	
@@ -215,6 +235,8 @@ int return_player_position(int choice, int* y, int*x){
 			*x = 2;
 			break;
 		case 6:
+			*y = 3;
+			*x = 3;
 			break;
 		default:
 			break;
@@ -231,7 +253,27 @@ int set_dice_in_packet(Player_packet* packet, int dice_value, int choice_value){
 }
 
 int set_player_in_packet(Player_packet* packet, int player_id, int y, int x){
+	unsigned short position_bit;
+	switch(player_id)
+	{
+		case 0:
+			position_bit = 0x0fff;
+			break;
+		case 1:
+			position_bit = 0xf0ff;
+			break;
+		case 2:
+			position_bit = 0xff0f;
+			break;
+		case 3:
+			position_bit = 0xfff0;
+			break;
+		default:
+			break;
+	}
+
 	x <<= 2;
+	packet->position &= position_bit;
 	packet->position |= (unsigned short)(x << ((3 - player_id) * 4));
 	packet->position |= (unsigned short)(y << ((3 - player_id) * 4));
 	return 0;
@@ -241,10 +283,9 @@ int game_init(int sock, int *player_id, WINDOW ***windows, Player_packet *packet
 
 	packet_recv(sock,(char*)packet, NULL);
 	
-	windows = display_init(packet->cards);	 
-
 	*player_id = PLAYER_ID(packet->info);
 	
+	windows = display_init(packet->cards,*player_id);	 
 	return 0;
 }
 
@@ -264,14 +305,17 @@ int game_update(int sock, WINDOW ***windows, Cursor *cursor){
 	int turn_player = PLAYER_TURN_PLAYER(packet.info) >> 4;
 	int room_num = PLAYER_SELECT_VALUE(packet.dice) ;
 
-	WINDOW **player;
-	return_player_horse(windows,&player,turn_player); // 플레이어 윈도우 반환
-	horse_update(player,room_num,turn_player); // 위에서 반환받은 윈도우에 말 표시
+	if( room_num !=7){
+		WINDOW **player;
+		return_player_horse(windows,&player,turn_player); // 플레이어 윈도우 반환
+		horse_update(player,room_num,turn_player); // 위에서 반환받은 윈도우에 말 표시
+	}
 
 	char buf[128];
 	int turn_player_dice = (int)PLAYER_SELECT_VALUE(packet.dice);
-	sprintf(buf,"player %d: dice is : %d. I go to  %s room ",turn_player,turn_player_dice,parse_room_name(room_num));
-	str_add(cursor->log_str,((cursor->log_cnt)+=1),buf);
+	sprintf(buf,"player %d went %s room",turn_player,parse_room_name(room_num));
+	str_add(cursor->log_str,(cursor->log_cnt)++,buf);
+	history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 	
 	return 0;	
 }
@@ -326,7 +370,8 @@ int send_clue(int sock, Player_packet *packet,Cursor *cursor,WINDOW ***windows) 
 			}
 			char buf[128];
 			sprintf(buf,"Invalid clue.");
-			str_add(cursor->log_str,((cursor->log_cnt)+=1),buf);
+			str_add(cursor->log_str,(cursor->log_cnt)++,buf);
+	history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 		}
 
 		packet->clue = select_clue;
@@ -366,7 +411,8 @@ int print_clue(Player_packet *packet,Cursor *cursor) {
 	// 아무도 단서를 내지 않은 경우
 	if (packet->clue == 0) {
 		sprintf(clue_buf,"no one has it"); 
-		str_add(cursor->log_str,((cursor->log_cnt)+=1),clue_buf);
+		str_add(cursor->log_str,(cursor->log_cnt)++,clue_buf);
+		history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 		//printf("아무도 단서를 내지 않음!!\n");
 	}
 	// 턴플레이어가 아닌 다른 플레이어가 단서를 제출한 경우
@@ -376,21 +422,23 @@ int print_clue(Player_packet *packet,Cursor *cursor) {
 		}
 		else {
 			sprintf(clue_buf,"player%d: send to player%d",clue_player_id,turn_player_id); 
-			str_add(cursor->log_str,((cursor->log_cnt)+=1),clue_buf);
+			str_add(cursor->log_str,(cursor->log_cnt)++,clue_buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 			//printf("%d 플레이어가 %d 플레이어에게 단서를 제출함!!\n", clue_player_id, turn_player_id);
 		}
 	}
 	// 턴플레이어가 단서를 제출한 경우
 	else {
 		sprintf(clue_buf,"turn player(%d) send %s",clue_player_id,clue_str); 
-		str_add(cursor->log_str,((cursor->log_cnt)+=1),clue_buf);
+		str_add(cursor->log_str,(cursor->log_cnt)++,clue_buf);
+		history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 		//printf("%d(턴플레이어)가 자신의 카드 %d을 제출!!\n", turn_player_id, clue);
 	}
 
 	return 0;
 }
 
-int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
+int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows, pthread_t pid) {
 	
 	int sig;
 	int type;
@@ -398,8 +446,14 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 	// 턴플은 SIG_INFR, 나머지는 SIG_WAIT을 기다림
 	packet_recv(sock, (char*)&sig, NULL);
 
+
 	// 플레이어가 SIG_INFR를 받은 경우 : 턴플레이어
 	if (sig == SIG_INFR) {
+		
+		char infer_buf[128];
+		sprintf(infer_buf,"player %d start ",player_id);
+		str_add(cursor->log_str,(cursor->log_cnt)++,infer_buf);
+		history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 
 		// 범인, 흉기(장소는 이미 그 자리로 이동을 했으므로 자동 카운트)를 
 		// 정하는 함수를 만든뒤 패킷에 담는다. 그런 다음에 packet_send를 한다.
@@ -415,15 +469,17 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 		position_bit >>= (4 * (3 - player_id));
 
 		// 진실의 방(x:3, y:3)이라면
-		if(position_bit == 0xF)
-		{
-
-		}
 
 		short scene, crime, weapon;
 		while(1){	
 			/// 진실의 방이 아닌 경우 인자가 1인 함수 실행 안하고 셀렉트 다이스 값으로 room을 셋팅합니다.
-			if ((scene = infer_cursor(1)) == -1) { continue; }
+			if(position_bit == 0xF)
+			{
+				if ((scene = infer_cursor(1)) == -1) { continue; }
+			}
+			else{
+				scene = PLAYER_SELECT_VALUE(packet.dice); 
+			}
 			if ((crime = infer_cursor(2)) == -1) { continue; }
 			if ((weapon = infer_cursor(3)) == -1) { continue; }
 			break;
@@ -433,6 +489,8 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 
 		// 자신의 추리 정보를 서버에 전송
 		type = PACKET;
+		
+
 		if (packet_send(sock, (char*)&packet, &type) == -1){
 			return -1;
 		}
@@ -447,12 +505,14 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 		int crime_card = PARSE_CARD(crime);
 		int weapon_cate = PARSE_CATE(weapon);
 		int weapon_card = PARSE_CARD(weapon);
-		char *scene_str = parse_card(scene_cate,scene_card);
-		char *crime_str = parse_card(crime_cate,crime_card);
-		char *weapon_str = parse_card(weapon_cate,weapon_card);
+		char *scene_str = parse_card(scene_cate, scene_card);
+		char *crime_str = parse_card(crime_cate, crime_card);
+		char *weapon_str = parse_card(weapon_cate, weapon_card);
+
 		char buf[128];
-		sprintf(buf,"player %d: scene : %s, crime : %s, weapon: %s",player_id,scene_str,crime_str,weapon_str);
-		str_add(cursor->history_str,((cursor->history_cnt)+=1),buf);
+		sprintf(buf,"your infer : %s killed him with %s in %s", crime_str, weapon_str, scene_str);
+		str_add(cursor->history_str,(cursor->history_cnt)++,buf);
+		history_log_print(cursor->history, cursor->history_str, cursor->history_cnt, sizeof(cursor->history_str)/sizeof(cursor->history_str[0]));
 
 		// 서버로부터 전송되는 단서 신호를 대기
 		// SIG_TURN or SIG_DONE
@@ -468,14 +528,16 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 			if(packet_recv(sock, (char*)&packet, NULL) == -1){
 				return -1;
 			}
-			char infer_buf[128];
 			int clue_player = (int)PLAYER_CLUE_PLAYER(packet.clue)>>5;// 클루 플레이어 뽑아내기
 			char clue_item = PLAYER_CLUE(packet.clue); // 단서 뽑아내기
 			int clue_cate = PARSE_CATE(clue_item);
 			int clue_card = PARSE_CARD(clue_item);
+
+			char infer_buf[128];
 			char* clue_name = parse_card(clue_cate,clue_card);
-			sprintf(infer_buf,"player%d: (clue : %s) -> player%d",clue_player,clue_name,player_id); 
-			str_add(cursor->log_str,((cursor->log_cnt)+=1),infer_buf);
+			sprintf(infer_buf,"player%d give a clue \" %s \" ",clue_player,clue_name); 
+			str_add(cursor->log_str,(cursor->log_cnt)++,infer_buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 
 			// UI 처리(단서 정보를 플레이어 화면에 출력)
 			print_clue(&packet,cursor);
@@ -483,6 +545,12 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 		// SIG_TURN이 전송됨
 		// 아무도 단서를 내지 않아 턴플이 단서를 내야되는 경우
 		else if (sig == SIG_TURN) { 
+
+			char infer_buf[128];
+			sprintf(infer_buf," no one has clue. you have to show clue everyone "); 
+			str_add(cursor->log_str,(cursor->log_cnt)++,infer_buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
+
 			send_clue(sock, &packet, cursor, windows);
 
 			// UI 처리(단서 정보를 모든 플레이어 로그 화면에 출력)
@@ -491,21 +559,35 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 		}
 		// SIG_DIE가 전송됨
 		else if (sig == SIG_DIE) {
+			char infer_buf[128];
+			sprintf(infer_buf," you died .... "); 
+			str_add(cursor->log_str,(cursor->log_cnt)++,infer_buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
+
 			// 죽은 플레이어의 카드가 공개됨. 안쓰지만 패킷은 받아두자!
 			packet_recv(sock, (char*)&packet, NULL);
 			return SIG_DIE;
 		}
 		// SIG_WIN이 전송됨
-		else {
-			printf("%d플레이어 승리!!\n", PLAYER_TURN_PLAYER(packet.info) >> 4);
+		else {     
+			char buf[128];
+			sprintf(buf,"you win!");
+			str_add(cursor->log_str,(cursor->log_cnt)++,buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 			return SIG_WIN;
 		}
+
 	}
 
 	// 플레이어가 SIG_WAIT을 받은 경우 : 턴플레이어가 아닌 다른 플레이어
-	else {    
+	else{   
+
 		Player_packet packet = {0,};
 
+		char buf1[128];
+		sprintf(buf1,"please wait for inference");
+		str_add(cursor->log_str,(cursor->log_cnt)++,buf1);
+		history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 		// 서버로부터 라우팅된 추리정보가 담긴 턴플레이어의 패킷을 받음
 		if (packet_recv(sock, (char*)&packet, NULL) == -1){
 			return -1;
@@ -517,38 +599,54 @@ int game_infer(int sock, int player_id, Cursor* cursor, WINDOW ***windows) {
 			return -1;
 		}
 
+		pthread_cancel(pid);
+
 		// 만약 SIG_TURN이 왔다면 내가 단서를 서버에 제출해야함
 		// 만약 SIG_DONE이 왔다면 누가 이미 냈으므로 너는 넘어가라는 의미
 		// 만약 SIG_DIE가 왔다면 턴플레이어가 죽음
 		// 만약 SIG_WIN이 왔다면 게임이 끝남
 
 		// SIG_TURN 을 받음
+		char buf[BUFSIZ];
 		if(sig == SIG_TURN){
+			sprintf(buf," hey! send your clue ");
+			str_add(cursor->log_str,(cursor->log_cnt)++,buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
+
 			send_clue(sock, &packet,cursor, windows);
 			print_clue(&packet,cursor);
 			return 0;
 		}
 		// SIG_DONE 을 받음
 		else if (sig == SIG_DONE) { 
-			// 서버로부터 단서 패킷을 받음
+			// 서버로부터 단서 패킷을 받
 			if(packet_recv(sock, (char *)&packet, NULL) == -1){
 				return -1;
 			}
+			char clue_player = PLAYER_CLUE_PLAYER(packet.clue)>>5;
+			char turn_player = PLAYER_TURN_PLAYER(packet.info)>>4;
+			sprintf(buf,"player%d gave a clue to player%d",clue_player,turn_player);
+			str_add(cursor->log_str,(cursor->log_cnt)++,buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 			print_clue(&packet, cursor);
 		}
 		else if (sig == SIG_DIE) {
-			printf("%d 플레이어가 죽었습니다!\n", PLAYER_TURN_PLAYER(packet.info) >> 4);
+			sprintf(buf,"player %d die!\n", PLAYER_TURN_PLAYER(packet.info) >> 4);
+			str_add(cursor->log_str,(cursor->log_cnt)++,buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
+
 			// 죽은 플레이어의 카드를 공개하기 위해 패킷을 받음
 			packet_recv(sock, (char*)&packet, NULL);
 			// 모든 플레이어 화면에 출력해야함
+			print_clue(&packet, cursor);
+			return SIG_DIE;
 		}
 		else {
-			printf("%d 플레이어가 죽었습니다!\n", PLAYER_TURN_PLAYER(packet.info) >> 4);
+			sprintf(buf,"player %d win!\n", PLAYER_TURN_PLAYER(packet.info) >> 4);
+			str_add(cursor->log_str,(cursor->log_cnt)++,buf);
+			history_log_print(cursor->log, cursor->log_str, cursor->log_cnt, sizeof(cursor->log_str)/sizeof(cursor->log_str[0]));
 			return SIG_WIN;
 		}
 	}
 	return 0;
 }
-
-
-
